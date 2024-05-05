5832cc5b1be46eb36615986e7918f1bcf5e601e3
Nathan <145821197+eunathan098@users.noreply.github.com>
2024-05-05 16:31:41 -0300

    correcao de bugs

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-<<<<<<< HEAD
 function acessar(){
 
     window.location.href="index2.html";
-
 }
-
-
-
-
-
-
-=======
-function acessar(){
-
-    window.location.href="index2.html";
-
-}
-
-
-
-
-
-
->>>>>>> 167168123640028f80e65b55abd835cb6e5dcb8a
   
